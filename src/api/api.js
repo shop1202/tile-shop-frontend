35b5565b592405084d25dc7555288c158a02e24b
shop1202 <alikkhatum@gmail.com>
2025-07-22 11:02:11 +0530

    fix: update .env for Vite compatibility

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`, // reads from .env
+  baseURL: `${import.meta.env.VITE_REACT_APP_API_URL}/api`,
 });
 
 // Purchase
